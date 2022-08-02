4672aafd5815cb947b391cd0d3362fd2e8abe273
Andraš Tšitškan <36357686+andras-tsitskan@users.noreply.github.com>
2022-08-02 11:00:23 +0300

    Add initial js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${
+    this.isRead ? "already read" : "not read yet"
+  }`;
+};
+
+const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 295, true);
+
+console.log(theHobbit.info());
+
+function addBookToLibrary() {}

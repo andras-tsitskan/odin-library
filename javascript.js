3291156a49d583a4a2a90f3ed09067a8c62bff2c
Andraš Tšitškan <36357686+andras-tsitskan@users.noreply.github.com>
2022-08-03 12:18:20 +0300

    Create dummy content books

--- javascript.js
+++ javascript.js
@@ -15,8 +15,38 @@ Book.prototype.info = function () {
   }`;
 };
 
-const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 295, true);
+// Add initial dummy content to simulate existing storage.
 
-console.log(theHobbit.info());
+const book1 = new Book(
+  "And Then There Were None",
+  "Agatha Christie",
+  264,
+  false
+);
+const book2 = new Book(
+  "The Little Prince",
+  "Antoine de Saint-Exupéry",
+  96,
+  true
+);
+const book3 = new Book("Dune", "Frank Herbert", 658, false);
+const book4 = new Book(
+  "Murder on the Orient Express",
+  "Agatha Christie",
+  274,
+  true
+);
+const book5 = new Book(
+  "The Old Man and the Sea",
+  "Ernest Hemingway",
+  96,
+  false
+);
+const book6 = new Book("To Kill a Mockingbird", "Harper Lee", 336, true);
 
-function addBookToLibrary() {}
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
+myLibrary.push(book4);
+myLibrary.push(book5);
+myLibrary.push(book6);
